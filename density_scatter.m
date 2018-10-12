e378d1e14e09d616a8ceb901b87921589fb828f3
whuer_pcy <p_c_yuan@163.com>
2018-10-12 23:48:22 +0800

    Create density_scatter.m

---
+++ density_scatter.m
@@ -0,0 +1,25 @@
+function [D]=denscatter(A,B,R,dotsize)
+      %% A and B are columns vectors, R is the radious for count dots since current dot
+      if nargin<3
+          R=10;
+          dotsize=20;
+      else if nargin<4
+              dotsize=20;
+          end
+      end
+      AB=[A B];
+      distmat=squareform(pdist(AB));
+      C=sum(distmat<=R,2);
+      D=max(C);
+      scatter(A,B,dotsize,C,'filled');
+      %some configuration
+      colormap('jet');
+      cb=colorbar();
+      colormap(cb,'jet');
+      set(cb,'FontSize',12);
+      cb.Label.String='Counts';
+      rl=refline([1,0]);
+      rl.LineWidth=2;
+      rl.Color='k';
+      set(gca,'FontSize',16);
+end
